6321fb8d18d9ac00d05ab4f9767d3ebaad74b1c8
Oleksiy Krivoshey <oleksiy@luckyteam.co.uk>
2016-01-05 15:02:46 +0200

    An option to overwrite starting offset

--- test/03.group_consumer.js
+++ test/03.group_consumer.js
@@ -9,25 +9,45 @@ var Kafka   = require('../lib/index');
 var _       = require('lodash');
 
 var producer = new Kafka.Producer({requiredAcks: 1});
-var consumer = new Kafka.GroupConsumer({
-    idleTimeout: 100,
-    heartbeatTimeout: 100
-});
+var consumers = [
+    new Kafka.GroupConsumer({
+        idleTimeout: 100,
+        heartbeatTimeout: 100
+    }),
+    new Kafka.GroupConsumer({
+        idleTimeout: 100,
+        heartbeatTimeout: 100
+    }),
+    new Kafka.GroupConsumer({
+        idleTimeout: 100,
+        heartbeatTimeout: 100
+    }),
+    new Kafka.GroupConsumer({
+        idleTimeout: 100,
+        heartbeatTimeout: 100,
+        startFrom: {
+            time: Kafka.EARLIEST_OFFSET
+        }
+    })
+];
 
-var dataListenerSpy = sinon.spy(function(messageSet, topic, partition) {
-    messageSet.forEach(function (m) {
-        consumer.commitOffset({topic: topic, partition: partition, offset: m.offset});
-    });
-});
+var dataListenerSpies = [
+    sinon.spy(function (messageSet, topic, partition) {messageSet.forEach(function (m) {consumers[0].commitOffset({topic: topic, partition: partition, offset: m.offset}); });}),
+    sinon.spy(function (messageSet, topic, partition) {messageSet.forEach(function (m) {consumers[1].commitOffset({topic: topic, partition: partition, offset: m.offset}); });}),
+    sinon.spy(function (messageSet, topic, partition) {messageSet.forEach(function (m) {consumers[2].commitOffset({topic: topic, partition: partition, offset: m.offset}); });}),
+    sinon.spy(function (messageSet, topic, partition) {messageSet.forEach(function (m) {consumers[3].commitOffset({topic: topic, partition: partition, offset: m.offset}); });}),
+];
 
-consumer.on('data', dataListenerSpy);
+consumers.forEach(function (c, i) {
+    c.on('data', dataListenerSpies[i]);
+});
 
 describe('GroupConsumer', function () {
     before(function () {
         this.timeout(6000); // let Kafka create offset topic
         return Promise.all([
             producer.init(),
-            consumer.init({
+            consumers[0].init({
                 strategy: 'TestStrategy',
                 subscriptions: ['kafka-test-topic'],
                 fn: Kafka.GroupConsumer.RoundRobinAssignment
@@ -36,7 +56,7 @@ describe('GroupConsumer', function () {
     });
 
     it('required methods', function () {
-        return consumer.should
+        return consumers[0].should
             .respondTo('init')
             .respondTo('subscribe')
             .respondTo('offset')
@@ -46,7 +66,7 @@ describe('GroupConsumer', function () {
     });
 
     it('should receive new messages', function () {
-        dataListenerSpy.reset();
+        dataListenerSpies[0].reset();
         return producer.send({
             topic: 'kafka-test-topic',
             partition: 0,
@@ -55,20 +75,20 @@ describe('GroupConsumer', function () {
         .delay(200)
         .then(function () {
             /* jshint expr: true */
-            dataListenerSpy.should.have.been.called;
-            dataListenerSpy.lastCall.args[0].should.be.an('array').and.have.length(1);
-            dataListenerSpy.lastCall.args[1].should.be.a('string', 'kafka-test-topic');
-            dataListenerSpy.lastCall.args[2].should.be.a('number', 0);
+            dataListenerSpies[0].should.have.been.called;
+            dataListenerSpies[0].lastCall.args[0].should.be.an('array').and.have.length(1);
+            dataListenerSpies[0].lastCall.args[1].should.be.a('string', 'kafka-test-topic');
+            dataListenerSpies[0].lastCall.args[2].should.be.a('number', 0);
 
-            dataListenerSpy.lastCall.args[0][0].should.be.an('object');
-            dataListenerSpy.lastCall.args[0][0].should.have.property('message').that.is.an('object');
-            dataListenerSpy.lastCall.args[0][0].message.should.have.property('value');
-            dataListenerSpy.lastCall.args[0][0].message.value.toString('utf8').should.be.eql('p00');
+            dataListenerSpies[0].lastCall.args[0][0].should.be.an('object');
+            dataListenerSpies[0].lastCall.args[0][0].should.have.property('message').that.is.an('object');
+            dataListenerSpies[0].lastCall.args[0][0].message.should.have.property('value');
+            dataListenerSpies[0].lastCall.args[0][0].message.value.toString('utf8').should.be.eql('p00');
         });
     });
 
     it('should be able to commit offsets', function () {
-        return consumer.commitOffset([
+        return consumers[0].commitOffset([
             {
                 topic: 'kafka-test-topic',
                 partition: 0,
@@ -95,7 +115,7 @@ describe('GroupConsumer', function () {
     });
 
     it('should be able to fetch commited offsets', function () {
-        return consumer.fetchOffset([
+        return consumers[0].fetchOffset([
         {
             topic: 'kafka-test-topic',
             partition: 0
@@ -127,9 +147,9 @@ describe('GroupConsumer', function () {
 
     it('offset() should return last offset', function () {
         return Promise.all([
-            consumer.offset('kafka-test-topic', 0),
-            consumer.offset('kafka-test-topic', 1),
-            consumer.offset('kafka-test-topic', 2),
+            consumers[0].offset('kafka-test-topic', 0),
+            consumers[0].offset('kafka-test-topic', 1),
+            consumers[0].offset('kafka-test-topic', 2),
         ]).spread(function (offset0, offset1, offset2) {
             offset0.should.be.a('number').and.be.gt(0);
             offset1.should.be.a('number').and.be.gt(0);
@@ -137,44 +157,22 @@ describe('GroupConsumer', function () {
 
             // commit them back for next tests
             return Promise.all([
-                consumer.commitOffset({topic: 'kafka-test-topic', partition: 0, offset: offset0-1}),
-                consumer.commitOffset({topic: 'kafka-test-topic', partition: 1, offset: offset1-1}),
-                consumer.commitOffset({topic: 'kafka-test-topic', partition: 2, offset: offset2-1})
+                consumers[0].commitOffset({topic: 'kafka-test-topic', partition: 0, offset: offset0-1}),
+                consumers[0].commitOffset({topic: 'kafka-test-topic', partition: 1, offset: offset1-1}),
+                consumers[0].commitOffset({topic: 'kafka-test-topic', partition: 2, offset: offset2-1})
             ]);
         });
     });
 
     it('should split partitions in a group', function () {
-        var consumer2 = new Kafka.GroupConsumer({
-            idleTimeout: 100,
-            heartbeatTimeout: 100
-        }), consumer3 = new Kafka.GroupConsumer({
-            idleTimeout: 100,
-            heartbeatTimeout: 100
-        });
-
-        var dataListenerSpy2 = sinon.spy(function(messageSet, topic, partition) {
-            messageSet.forEach(function (m) {
-                consumer2.commitOffset({topic: topic, partition: partition, offset: m.offset});
-            });
-        });
-
-        var dataListenerSpy3 = sinon.spy(function(messageSet, topic, partition) {
-            messageSet.forEach(function (m) {
-                consumer3.commitOffset({topic: topic, partition: partition, offset: m.offset});
-            });
-        });
-
-        consumer2.on('data', dataListenerSpy2);
-        consumer3.on('data', dataListenerSpy3);
-
+        this.timeout(6000);
         return Promise.all([
-            consumer2.init({
+            consumers[1].init({
                 strategy: 'TestStrategy',
                 subscriptions: ['kafka-test-topic'],
                 fn: Kafka.GroupConsumer.RoundRobinAssignment
             }),
-            consumer3.init({
+            consumers[2].init({
                 strategy: 'TestStrategy',
                 subscriptions: ['kafka-test-topic'],
                 fn: Kafka.GroupConsumer.RoundRobinAssignment
@@ -182,7 +180,7 @@ describe('GroupConsumer', function () {
         ])
         .delay(500) // give some time to rebalance group
         .then(function () {
-            dataListenerSpy.reset();
+            dataListenerSpies[0].reset();
             return producer.send([
                 {
                     topic: 'kafka-test-topic',
@@ -203,35 +201,96 @@ describe('GroupConsumer', function () {
             .delay(200)
             .then(function () {
                 /* jshint expr: true */
-                dataListenerSpy.should.have.been.calledOnce;
-                dataListenerSpy.lastCall.args[0].should.be.an('array').and.have.length(1);
-                dataListenerSpy2.should.have.been.calledOnce;
-                dataListenerSpy2.lastCall.args[0].should.be.an('array').and.have.length(1);
-                dataListenerSpy3.should.have.been.calledOnce;
-                dataListenerSpy3.lastCall.args[0].should.be.an('array').and.have.length(1);
+                dataListenerSpies[0].should.have.been.calledOnce;
+                dataListenerSpies[0].lastCall.args[0].should.be.an('array').and.have.length(1);
+                dataListenerSpies[1].should.have.been.calledOnce;
+                dataListenerSpies[1].lastCall.args[0].should.be.an('array').and.have.length(1);
+                dataListenerSpies[2].should.have.been.calledOnce;
+                dataListenerSpies[2].lastCall.args[0].should.be.an('array').and.have.length(1);
+            });
+        })
+        .then(function () {
+            // remove this consumer from the group because we will create new in next test and we need 1 consumer per partition
+            return consumers[2].end();
+        })
+        .delay(500); // rebalance group
+    });
+
+    it('should start from specified time/offset', function () {
+        dataListenerSpies.forEach(function (spy) {spy.reset()});
+        return consumers[3].init({
+            strategy: 'TestStrategy',
+            subscriptions: ['kafka-test-topic'],
+            fn: Kafka.GroupConsumer.RoundRobinAssignment
+        })
+        .delay(700) // give some time to rebalance group
+        .then(function () {
+            /* jshint expr: true */
+            dataListenerSpies[0].should.have.been.notCalled;
+            dataListenerSpies[1].should.have.been.notCalled;
+            dataListenerSpies[3].should.have.been.called;
+            // should receive all previous messages in partition
+            dataListenerSpies[3].lastCall.args[0].should.be.an('array').and.have.length.above(0);
+        });
+    });
+
+    it('should not use specified offset after rebalance but use committed', function () {
+        dataListenerSpies.forEach(function (spy) {spy.reset()});
+        return consumers[1].end()
+        .delay(700) // rebalance
+        .then(function () {
+            /* jshint expr: true */
+            dataListenerSpies[0].should.have.been.notCalled;
+            dataListenerSpies[3].should.have.been.notCalled;
+        })
+        .then(function () {
+            return producer.send([
+                {
+                    topic: 'kafka-test-topic',
+                    partition: 0,
+                    message: {value: 'p00'}
+                },
+                {
+                    topic: 'kafka-test-topic',
+                    partition: 1,
+                    message: {value: 'p01'}
+                },
+                {
+                    topic: 'kafka-test-topic',
+                    partition: 2,
+                    message: {value: 'p02'}
+                }
+            ])
+            .delay(200)
+            .then(function () {
+                /* jshint expr: true */
+                dataListenerSpies[0].should.have.been.called;
+                dataListenerSpies[0].lastCall.args[0].should.be.an('array').and.have.length(1);
+                dataListenerSpies[3].should.have.been.called;
+                dataListenerSpies[3].lastCall.args[0].should.be.an('array').and.have.length(1);
             });
         });
     });
 
     it('should list groups', function () {
-        return consumer.listGroups().then(function (groups) {
+        return consumers[0].listGroups().then(function (groups) {
             groups.should.be.an('array').and.have.length(1);
             groups[0].should.be.an('object');
-            groups[0].should.have.property('groupId', consumer.options.groupId);
+            groups[0].should.have.property('groupId', consumers[0].options.groupId);
             groups[0].should.have.property('protocolType', 'consumer');
         });
     });
 
     it('should describe group', function () {
-        return consumer.describeGroup().then(function (group) {
+        return consumers[0].describeGroup().then(function (group) {
             group.should.be.an('object');
             group.should.have.property('error', null);
-            group.should.have.property('groupId', consumer.options.groupId);
+            group.should.have.property('groupId', consumers[0].options.groupId);
             group.should.have.property('state');
             group.should.have.property('protocolType', 'consumer');
             group.should.have.property('protocol', 'TestStrategy');
             group.should.have.property('members').that.is.an('array');
-            group.members.should.have.length(3);
+            group.members.should.have.length(2);
             group.members[0].should.be.an('object');
             group.members[0].should.have.property('memberId').that.is.a('string');
             group.members[0].should.have.property('clientId').that.is.a('string');
