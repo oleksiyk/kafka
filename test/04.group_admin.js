eb17ec05c57ea4e6fa905d1737ba9b7b86eb0a40
juliusza <julius@zaromskis.lt>
2017-03-06 13:35:17 +0200

    Add an ability to check consumer lag (#155)
    * Should list groups test would fail on kafka setups with existing groups. Search all groups instead just checking the first one.
    
    * Added fetchConsumerLag method
    
    * 3.1.1
    
    * * rename client.getOffset to something like getPartitionOffset
    * drop all changes to package.json file

--- test/04.group_admin.js
+++ test/04.group_admin.js
@@ -5,16 +5,16 @@
 var Promise = require('bluebird');
 var Kafka   = require('../lib/index');
 
-var admin = new Kafka.GroupAdmin({ clientId: 'admin' });
-var consumer = new Kafka.GroupConsumer({
-    groupId: 'no-kafka-admin-test-group',
-    timeout: 1000,
-    idleTimeout: 100,
-    heartbeatTimeout: 100,
-    clientId: 'group-consumer'
-});
-
 describe('GroupAdmin', function () {
+    var admin = new Kafka.GroupAdmin({ clientId: 'admin' });
+    var consumer = new Kafka.GroupConsumer({
+        groupId: 'no-kafka-admin-test-group',
+        timeout: 1000,
+        idleTimeout: 100,
+        heartbeatTimeout: 100,
+        clientId: 'group-consumer'
+    });
+
     before(function () {
         return Promise.all([
             admin.init(),
@@ -43,10 +43,16 @@ describe('GroupAdmin', function () {
 
     it('should list groups', function () {
         return admin.listGroups().then(function (groups) {
+            var found = false;
+
             groups.should.be.an('array').and.have.length.gt(0);
-            groups[0].should.be.an('object');
-            groups[0].should.have.property('groupId', consumer.options.groupId);
-            groups[0].should.have.property('protocolType', 'consumer');
+            groups.forEach(function (group) {
+                if (group.groupId === consumer.options.groupId) {
+                    group.should.have.property('protocolType', 'consumer');
+                    found = true;
+                }
+            });
+            found.should.equal(true);
         });
     });
 
@@ -72,6 +78,82 @@ describe('GroupAdmin', function () {
             group.members[0].memberAssignment.should.have.property('partitionAssignment').that.is.an('array');
             group.members[0].memberAssignment.partitionAssignment[0].should.have.property('topic', 'kafka-test-topic');
             group.members[0].memberAssignment.partitionAssignment[0].should.have.property('partitions').that.is.an('array', [0, 1, 2]);
+        });
+    });
+});
+
+describe('GroupAdmin', function () {
+    var admin = new Kafka.GroupAdmin({ clientId: 'admin' });
+    var consumer = new Kafka.GroupConsumer({
+        groupId: 'no-kafka-admin-test-group',
+        timeout: 1000,
+        idleTimeout: 100,
+        heartbeatTimeout: 100,
+        clientId: 'group-consumer'
+    });
+    var producer = new Kafka.Producer({
+        requiredAcks: 1,
+        clientId: 'producer'
+    });
+
+    after(function () {
+        return Promise.all([
+            admin.end(),
+            consumer.end(),
+            producer.end()
+        ]);
+    });
+
+    it('should fetch consumer lag', function () {
+        return new Promise(function (resolve) {
+            Promise.all([
+                admin.init(),
+                // Produce and consume a message in order to set
+                // the current offset for partition 0
+                consumer.init({
+                    subscriptions: ['kafka-test-topic'],
+                    metadata: 'consumer-metadata',
+                    handler: function (messageSet, topic, partition) {
+                        return Promise.each(messageSet, function (m) {
+                            // commit offset
+                            return consumer.commitOffset(
+                                {
+                                    topic: topic,
+                                    partition: partition,
+                                    offset: m.offset
+                                }
+                            );
+                        }).then(function () {
+                            // All messages consumed,
+                            // expecting lag to be 0 at this point
+                            resolve();
+                        });
+                    }
+                }),
+                producer.init()
+            ]).then(function () {
+                producer.send({
+                    topic: 'kafka-test-topic',
+                    partition: 0,
+                    message: {
+                        key: 'test-key',
+                        value: 'Hello!'
+                    }
+                });
+            });
+        }).then(function () {
+            return admin.fetchConsumerLag(consumer.options.groupId, [{
+                topicName: 'kafka-test-topic',
+                partitions: [0, 1, 2]
+            }]);
+        }).then(function (consumers) {
+            consumers.should.have.length(3, 'asked for 3 partitions, expecting 3 responses');
+            consumers[0].should.have.property('topic');
+            consumers[0].should.have.property('partition');
+            consumers[0].should.have.property('offset');
+            consumers[0].should.have.property('highwaterMark');
+            consumers[0].should.have.property('consumerLag');
+            consumers[0].consumerLag.should.be.equal(0, 'expecting no lag, all messages are consumed');
         });
     });
 });
